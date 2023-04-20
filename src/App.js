ae9db2dfa1c14ac4fcd66b5fd8b92f97d7f6b810
Dércio Fernandes <315236@via.dk>
2023-04-20 10:13:52 +0200

    Added Pagination 2

--- src/App.js
+++ src/App.js
@@ -7,29 +7,51 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      pokemons: []
+      pokemons: [],
+      currentPage: 1,
+      pokemonsPerPage: 10,
+      totalPages: null
     };
   }
 
   componentDidMount() {
-    fetch("https://pokeapi.co/api/v2/pokemon/?limit=10")
+    fetch("https://pokeapi.co/api/v2/pokemon/")
       .then(response => response.json())
       .then(data => {
-        let results = data.results;
-        let promisesArray = results.map(result => {
-          return fetch(result.url).then(response => response.json());
-        });
-        return Promise.all(promisesArray);
-      })
-      .then(data =>
-        this.setState({ pokemons: data }, () =>
-          console.log("Main Pokemon State: ", this.state.pokemons)
-        )
-      );
+        const totalPokemons = data.count;
+        const totalPages = Math.ceil(totalPokemons / this.state.pokemonsPerPage);
+        this.setState({ totalPages });
+
+        return fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${this.state.pokemonsPerPage}&offset=${(this.state.currentPage - 1) * this.state.pokemonsPerPage}`)
+          .then(response => response.json())
+          .then(data => {
+            let results = data.results;
+            let promisesArray = results.map(result => {
+              return fetch(result.url).then(response => response.json());
+            });
+            return Promise.all(promisesArray);
+          })
+          .then(data => {
+            this.setState({ pokemons: data }, () =>
+              console.log("Main Pokemon State: ", this.state.pokemons)
+            );
+          });
+      });
+  }
+
+  handlePageChange = (pageNumber) => {
+    this.setState({ currentPage: pageNumber }, () => {
+      this.componentDidMount();
+    });
   }
 
   render() {
-    return <PokeList pokemons={this.state.pokemons} />;
+    return (
+      <div>
+        <PokeList pokemons={this.state.pokemons} />
+        <Pagination currentPage={this.state.currentPage} totalPages={this.state.totalPages} onPageChange={this.handlePageChange} />
+      </div>
+    );
   }
 }
 
@@ -43,7 +65,7 @@ const PokeList = ({ pokemons }) => {
   return (
     <div>
       {pokemons.map(pokemon => (
-        <Pokemon name={pokemon.name} sprite={pokemon.sprites.front_default} types={pokemon.types} />
+        <Pokemon key={pokemon.id} name={pokemon.name} sprite={pokemon.sprites.front_default} types={pokemon.types} />
       ))}
     </div>
   );
@@ -57,5 +79,32 @@ export const Pokemon = ({ name, sprite, types }) => {
     </div>
   );
 };
+
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+  const pagesToShow = 2; // Number of pages to show before and after current page
+  const pageNumbers = [];
+
+  for (let i = Math.max(1, currentPage - pagesToShow); i <= Math.min(totalPages, currentPage + pagesToShow); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <nav>
+      <ul className="pagination">
+        {pageNumbers.map((number) => (
+          <li key={number} className={`page-item ${number === currentPage ? 'active' : ''}`}>
+            <button
+              onClick={() => onPageChange(number)}
+              className="page-link"
+            >
+              {number}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
 
 export default App;
