58b1f0575a3d399211a84ff3d16833471e77ea3b
Dércio Fernandes <315236@via.dk>
2023-04-20 10:13:37 +0200

    Added Pagination

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
+import('https://fonts.cdnfonts.com/css/pokemon-solid');
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pokemons: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://pokeapi.co/api/v2/pokemon/?limit=10")
+      .then(response => response.json())
+      .then(data => {
+        let results = data.results;
+        let promisesArray = results.map(result => {
+          return fetch(result.url).then(response => response.json());
+        });
+        return Promise.all(promisesArray);
+      })
+      .then(data =>
+        this.setState({ pokemons: data }, () =>
+          console.log("Main Pokemon State: ", this.state.pokemons)
+        )
+      );
+  }
+
+  render() {
+    return <PokeList pokemons={this.state.pokemons} />;
+  }
+}
+
+const PokeList = ({ pokemons }) => {
+  console.log(pokemons);
+
+  if (pokemons.length === 0) {
+    return <div>Loading ...</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {pokemons.map(pokemon => (
+        <Pokemon name={pokemon.name} sprite={pokemon.sprites.front_default} types={pokemon.types} />
+      ))}
     </div>
   );
-}
+};
+
+export const Pokemon = ({ name, sprite, types }) => {
+  return (
+    <div className={"poke-card " + types[0].type.name} >
+      {" "}
+      <img alt="Pokemon sprite" src={sprite} /> <h3>{name}</h3>{" "}
+    </div>
+  );
+};
 
 export default App;
